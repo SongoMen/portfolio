5f27b2cdce42b79bdfcf3bda749d38dbe0c89ae1
SongoMen <songomengaming@gmail.com>
2018-12-03 01:11:29 +0100

    animations,hash's,about

---
+++ main.js
@@ -0,0 +1,29 @@
+setTimeout(function(){
+  window.addEventListener('wheel', function(e) {
+    if (e.deltaY < 0 && window.location.hash=="#about") {
+      $(".home-left").css('width','50%')
+      $(".home-center").css('margin-left','0%')
+      $(".about").css('display','none')
+      window.location.hash=""
+    }
+    if (e.deltaY > 0 && window.location.hash=="") {
+      $(".home-left").css('width','100%')
+      $(".home-center").css('margin-left','100%')
+      $(".about").css('display','flex')
+      window.location.hash="#about"
+    }
+  });
+},1500)
+
+
+jQuery(document).ready(function(){
+  if (window.location.hash === "") {
+      $(".home").css({ display: 'flex' })
+  } else if (window.location.hash === '#about') {
+      $(".about").css({display: 'flex'})
+      $(".home-left").css('animation','leftSide2 1s')
+      $(".home-left").css('width','100%')
+      $(".home-center").css('margin-left','100%')
+      $(".home").css({ display: 'flex' })
+  }
+})
