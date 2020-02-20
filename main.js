e6fb59b833d4c70cd3468fcd0fd1548ec26beefb
SongoMen <songomengaming@gmail.com>
2020-02-20 23:35:23 +0100

    added smooth scroll

--- main.js
+++ main.js
@@ -20,6 +20,41 @@ function r(f) {
   /in/.test(document.readyState) ? setTimeout("r(" + f + ")", 9) : f();
 }
 
+function getElementY(query) {
+  return (
+    window.pageYOffset +
+    document.querySelector(query).getBoundingClientRect().top
+  );
+}
+
+function doScrolling(element, duration) {
+  var startingY = window.pageYOffset;
+  var elementY = getElementY(element);
+  var targetY =
+    document.body.scrollHeight - elementY < window.innerHeight
+      ? document.body.scrollHeight - window.innerHeight
+      : elementY;
+  var diff = targetY - startingY;
+  var easing = function(t) {
+    return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+  };
+  var start;
+  if (!diff) return;
+  window.requestAnimationFrame(function step(timestamp) {
+    if (!start) start = timestamp;
+    var time = timestamp - start;
+    var percent = Math.min(time / duration, 1);
+    percent = easing(percent);
+    window.scrollTo(0, startingY + diff * percent);
+    if (time < duration) {
+      window.requestAnimationFrame(step);
+    }
+  });
+}
+
+document
+  .getElementById("home-btn")
+  .addEventListener("click", doScrolling.bind(null, "#aboutsection", 1000));
 
 function closeMenu() {
   topMenu.style.marginTop = "-300%";
