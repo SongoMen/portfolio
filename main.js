68ccdbdf4930f2889dc9d636054a6b371aeee5e3
SongoMen <songomengaming@gmail.com>
2020-02-21 22:11:27 +0100

    page with my project

--- main.js
+++ main.js
@@ -22,6 +22,15 @@ function r(f) {
   /in/.test(document.readyState) ? setTimeout("r(" + f + ")", 9) : f();
 }
 
+function isScrolledIntoView(el) {
+  var rect = el.getBoundingClientRect();
+  var elemTop = rect.top;
+  var elemBottom = rect.bottom;
+
+  var isVisible = elemTop >= 0 && elemBottom <= window.innerHeight;
+  return isVisible;
+}
+
 function Bar() {
   var Scroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height =
@@ -63,10 +72,11 @@ function doScrolling(element, duration) {
     }
   });
 }
-
-document
-  .getElementById("home-btn")
-  .addEventListener("click", doScrolling.bind(null, "#aboutsection", 1000));
+if (document.getElementById("home-btn")) {
+  document
+    .getElementById("home-btn")
+    .addEventListener("click", doScrolling.bind(null, "#aboutsection", 1000));
+}
 
 function closeMenu() {
   if (menuStatus) {
@@ -93,83 +103,89 @@ r(function() {
     emailval = false,
     textval = false,
     thanks = false;
-  document.getElementById("form").action = "";
+  if (document.getElementById("form")) {
+    document.getElementById("form").action = "";
+  }
 
   var regex = new RegExp(
     '^(([^<>()[\\]\\\\.,;:\\s@\\"]+(\\.[^<>()[\\]\\\\.,;:\\s@\\"]+)*)|' +
       '(\\".+\\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])' +
       "|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$",
   );
+  if (document.querySelector(".form__input.email input")) {
+    document
+      .querySelector(".form__input.email input")
+      .addEventListener("keyup", function() {
+        this.parentElement.classList.toggle("success", regex.test(this.value));
+        if (
+          regex.test(document.querySelector(".form__input.email input").value)
+        ) {
+          emailval = true;
+        } else {
+          emailval = false;
+        }
+      });
 
-  document
-    .querySelector(".form__input.email input")
-    .addEventListener("keyup", function() {
-      this.parentElement.classList.toggle("success", regex.test(this.value));
-      if (
-        regex.test(document.querySelector(".form__input.email input").value)
-      ) {
-        emailval = true;
-      } else {
-        emailval = false;
+    document
+      .querySelector(".form__input.name input")
+      .addEventListener("keyup", function() {
+        this.parentElement.classList.toggle("success", this.value.length > 2);
+        if (
+          document.querySelector(".form__input.name input").value.length > 2
+        ) {
+          nameval = true;
+        } else {
+          nameval = false;
+        }
+      });
+
+    document
+      .querySelector(".form__input.textarea textarea")
+      .addEventListener("keyup", function() {
+        this.parentElement.classList.toggle("success", this.value.length > 10);
+        if (
+          document.querySelector(".form__input.textarea textarea").value
+            .length > 10
+        ) {
+          textval = true;
+        } else {
+          textval = false;
+        }
+      });
+
+    document.querySelector("#form").addEventListener("submit", function(e) {
+      if (!emailval || !nameval || !textval) {
+        e.preventDefault();
       }
-    });
-
-  document
-    .querySelector(".form__input.name input")
-    .addEventListener("keyup", function() {
-      this.parentElement.classList.toggle("success", this.value.length > 2);
-      if (document.querySelector(".form__input.name input").value.length > 2) {
-        nameval = true;
+      if (!emailval) {
+        document.querySelector(".form__input.email input").style.border =
+          "1px solid #ef233c";
       } else {
-        nameval = false;
+        document.querySelector(".form__input.email input").style.border =
+          "none";
       }
-    });
-
-  document
-    .querySelector(".form__input.textarea textarea")
-    .addEventListener("keyup", function() {
-      this.parentElement.classList.toggle("success", this.value.length > 10);
-      if (
-        document.querySelector(".form__input.textarea textarea").value.length >
-        10
-      ) {
-        textval = true;
+      if (!nameval) {
+        document.querySelector(".form__input.name input").style.border =
+          "1px solid #ef233c";
       } else {
-        textval = false;
+        document.querySelector(".form__input.name input").style.border = "none";
       }
+      if (!textval) {
+        document.querySelector(".form__input.textarea textarea").style.border =
+          "1px solid #ef233c";
+      } else {
+        document.querySelector(".form__input.textarea textarea").style.border =
+          "none";
+      }
+      if (nameval && emailval && textval) {
+        document.getElementById("form").action =
+          "https://getsimpleform.com/messages?form_api_token=d55de506b28707f8ee113a4e3e6c1539";
+        thanks = true;
+        return true;
+      }
+      return false;
     });
-
-  document.querySelector("#form").addEventListener("submit", function(e) {
-    if (!emailval || !nameval || !textval) {
-      e.preventDefault();
-    }
-    if (!emailval) {
-      document.querySelector(".form__input.email input").style.border =
-        "1px solid #ef233c";
-    } else {
-      document.querySelector(".form__input.email input").style.border = "none";
-    }
-    if (!nameval) {
-      document.querySelector(".form__input.name input").style.border =
-        "1px solid #ef233c";
-    } else {
-      document.querySelector(".form__input.name input").style.border = "none";
-    }
-    if (!textval) {
-      document.querySelector(".form__input.textarea textarea").style.border =
-        "1px solid #ef233c";
-    } else {
-      document.querySelector(".form__input.textarea textarea").style.border =
-        "none";
-    }
-    if (nameval && emailval && textval) {
-      document.getElementById("form").action =
-        "https://getsimpleform.com/messages?form_api_token=d55de506b28707f8ee113a4e3e6c1539";
-      thanks = true;
-      return true;
-    }
-    return false;
-  });
+  }
 
   //*******************************************************
   //***                     MENU                        ***
@@ -189,11 +205,25 @@ r(function() {
   document
     .getElementById("menu-about")
     .addEventListener("click", doScrolling.bind(null, "#aboutsection", 1000));
+  if (window.location.pathname.split("/").pop() === "portfolio.html") {
+    document
+      .getElementById("menu-portfolio")
+      .addEventListener(
+        "click",
+        doScrolling.bind(null, "#portfoliosection", 1000),
+      );
+  }
   document
     .getElementById("menu-contact")
     .addEventListener("click", doScrolling.bind(null, "#contactsection", 1000));
 
   window.onscroll = function() {
+    let elements = document.getElementsByClassName("portfolio__animation");
+    for (let i = 0; i < elements.length; i++) {
+      if (isScrolledIntoView(elements[i])) {
+        elements[i].style.width = "0";
+      }
+    }
     Bar();
   };
 
