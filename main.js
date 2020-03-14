d7d95a62a90b4d0d0e3aa4b68b9bdd4834292f1e
SongoMen <pat.kozlowski2000@onet.pl>
2020-03-14 13:33:29 +0100

    fixes and improvments

--- main.js
+++ main.js
@@ -24,14 +24,11 @@ function r(f) {
 }
 
 function isElementInViewport(el) {
-  // Special bonus for those using jQuery
-
   var rect = el.getBoundingClientRect();
-
   return (
-    rect.top >= 0 &&
+    rect.top + 500>= 0 &&
     rect.left >= 0 &&
-    rect.bottom <=
+    rect.bottom - 500<=
       (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
